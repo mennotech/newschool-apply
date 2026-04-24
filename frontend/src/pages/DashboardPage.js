d56a9a77fe9f021a62d5472ebb35eca8a24268e2
Roland Penner <roland@mennotech.ca>
2026-04-23 22:32:29 -0500

    fix: Remove unused logoutUser import from DashboardPage.js

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { logoutUser } from '../store/slices/authSlice';
 import { clearApplication, deleteApplication, fetchApplications, setCurrentApplication } from '../store/slices/applicationSlice';
 
 const STATUS_LABELS = {
